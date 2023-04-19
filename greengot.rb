a95387ddc8cff12700e57db8ea148f3d3a37cae0
Grégoire Seux <grego_github@familleseux.net>
2023-04-19 10:41:34 +0200

    💄 Lint with rubocop -A
    Change-Id: I8a2dd62210bef91a0da500456f5be7789bfb15e3

--- greengot.rb
+++ greengot.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'net/http'
 require 'uri'
 require 'json'
@@ -22,15 +24,14 @@ class GreenGotClient
 
       client = GreenGotClient.new(id_token: auth['id_token'], device_id: auth['device_id'], auth_file: auth_file)
       client.check_minimum_version!
-      client
     else
       puts "No file at #{auth_file}, will go through the auth. ⚠ This will unregister your phone"
       client = GreenGotClient.new(id_token: nil, device_id: SecureRandom.uuid, auth_file: auth_file)
       client.check_minimum_version!
       client.interactive_signin_process
       client.save_auth
-      client
     end
+    client
   end
 
   def save_auth
@@ -40,54 +41,56 @@ class GreenGotClient
 
   # @throw if api version does not support us anymore
   def check_minimum_version!
-    uri = URI.parse("https://api.green-got.com/minimumVersion")
+    uri = URI.parse('https://api.green-got.com/minimumVersion')
     request = Net::HTTP::Get.new(uri)
-    request["X-Mobile-Unique-Id"] = @device_id
-    request["User-Agent"] = USER_AGENT
+    request['X-Mobile-Unique-Id'] = @device_id
+    request['User-Agent'] = USER_AGENT
 
     req_options = {
-      use_ssl: uri.scheme == "https",
+      use_ssl: uri.scheme == 'https'
     }
 
     response = Net::HTTP.start(uri.hostname, uri.port, req_options) do |http|
       http.request(request)
     end
 
-
     case response.code.to_i
     when 200
       supported_version = Gem::Version.new(JSON.parse(response.body)['minimumVersion'])
-      raise "Our version (#{APP_VERSION}) is not supported, time to re-explore API routes" unless APP_VERSION >= supported_version
-      puts "Our version is supported by the API 👌"
+      unless APP_VERSION >= supported_version
+        raise "Our version (#{APP_VERSION}) is not supported, time to re-explore API routes"
+      end
+
+      puts 'Our version is supported by the API 👌'
     else
-      raise "Unable to fetch minimal version supported by the API"
+      raise 'Unable to fetch minimal version supported by the API'
     end
   end
 
   def interactive_signin_process
-    print "Enter email address: "
-    email_address = STDIN.gets.strip
+    print 'Enter email address: '
+    email_address = $stdin.gets.strip
     signin(email_address)
 
     puts "You should receive an email to #{email_address} within a few seconds"
-    print "Enter confirmation code: "
-    confirmation_code = STDIN.gets.strip
-    print "Enter last 4 digits of credit card: "
-    last4digits = STDIN.gets.strip
+    print 'Enter confirmation code: '
+    confirmation_code = $stdin.gets.strip
+    print 'Enter last 4 digits of credit card: '
+    last4digits = $stdin.gets.strip
     @id_token = check_login_code(email_address, confirmation_code, last4digits)
-    puts "Successfuly connected to greengot account"
+    puts 'Successfuly connected to greengot account'
   end
 
   def signin(email_address)
-    uri = URI.parse("https://api.green-got.com/v2/signin")
+    uri = URI.parse('https://api.green-got.com/v2/signin')
     request = Net::HTTP::Post.new(uri)
-    request.content_type = "application/json"
-    request["X-Mobile-Unique-Id"] = @device_id
-    request["User-Agent"] = USER_AGENT
+    request.content_type = 'application/json'
+    request['X-Mobile-Unique-Id'] = @device_id
+    request['User-Agent'] = USER_AGENT
     request.body = JSON.dump(email: email_address)
 
     req_options = {
-      use_ssl: uri.scheme == "https",
+      use_ssl: uri.scheme == 'https'
     }
 
     response = Net::HTTP.start(uri.hostname, uri.port, req_options) do |http|
@@ -96,7 +99,7 @@ class GreenGotClient
 
     case response.code.to_i
     when 429
-      puts "Error while doing signin process because we issue too many requests, will sleep 60s and retry"
+      puts 'Error while doing signin process because we issue too many requests, will sleep 60s and retry'
       6.times do
         sleep(10)
         print '.'
@@ -106,42 +109,46 @@ class GreenGotClient
     when 200
       JSON.parse(response.body)
     else
-    raise "Error in signin process, code was #{response.code}. Body: #{response.body}"
+      raise "Error in signin process, code was #{response.code}. Body: #{response.body}"
     end
   end
 
   def check_login_code(email_address, confirmation_code, last4digits)
-    uri = URI.parse("https://api.green-got.com/v2/check-login-code")
+    uri = URI.parse('https://api.green-got.com/v2/check-login-code')
     request = Net::HTTP::Post.new(uri)
-    request.content_type = "application/json"
-    request["X-Mobile-Unique-Id"] = @device_id
-    request["User-Agent"] = USER_AGENT
+    request.content_type = 'application/json'
+    request['X-Mobile-Unique-Id'] = @device_id
+    request['User-Agent'] = USER_AGENT
     request.body = JSON.dump(email: email_address, oneTimeCode: confirmation_code, panLast4Digits: last4digits)
 
     req_options = {
-      use_ssl: uri.scheme == "https",
+      use_ssl: uri.scheme == 'https'
     }
 
     response = Net::HTTP.start(uri.hostname, uri.port, req_options) do |http|
       http.request(request)
     end
 
-    raise "Error in checking one time code, code was #{response.code}. Body: #{response.body}" unless response.code.to_i == 200
+    unless response.code.to_i == 200
+      raise "Error in checking one time code, code was #{response.code}. Body: #{response.body}"
+    end
+
     response_body = JSON.parse(response.body)
-    raise "Unable to find id" unless response_body.key?('idToken')
+    raise 'Unable to find id' unless response_body.key?('idToken')
+
     response_body['idToken']
   end
 
   def auth_get(path)
     uri = URI.parse("https://api.green-got.com/#{path}")
     request = Net::HTTP::Get.new(uri)
-    request.content_type = "application/json"
-    request["X-Mobile-Unique-Id"] = @device_id
-    request["authorization"] = "Bearer #{@id_token}"
-    request["User-Agent"] = USER_AGENT
+    request.content_type = 'application/json'
+    request['X-Mobile-Unique-Id'] = @device_id
+    request['authorization'] = "Bearer #{@id_token}"
+    request['User-Agent'] = USER_AGENT
 
     req_options = {
-      use_ssl: uri.scheme == "https",
+      use_ssl: uri.scheme == 'https'
     }
 
     response = Net::HTTP.start(uri.hostname, uri.port, req_options) do |http|
@@ -167,16 +174,16 @@ class GreenGotClient
   def get_transactions(**query_params)
     # here we assume query params are correctly encoded, no verification is done
     query_params = { limit: 50 }.merge(query_params)
-    response = auth_get("/v2/transactions?#{query_params.map { |k,v| "#{k}=#{v}"}.join('&')}")
+    response = auth_get("/v2/transactions?#{query_params.map { |k, v| "#{k}=#{v}" }.join('&')}")
     return [] if response['transactions'].empty? || response['nextCursor'].nil?
 
     response['transactions'] + get_transactions(cursor: response['nextCursor'], startDate: response['nextStartDate'])
   end
 end
 
-client = GreenGotClient.load(File.join(ENV['HOME'], ".config/greengot/auth.json"))
+client = GreenGotClient.load(File.join(ENV['HOME'], '.config/greengot/auth.json'))
 client.user_info
 
 all_transactions = client.get_transactions
-STDERR.puts "Found #{all_transactions.size} transactions in this history"
+warn "Found #{all_transactions.size} transactions in this history"
 puts all_transactions.to_json
