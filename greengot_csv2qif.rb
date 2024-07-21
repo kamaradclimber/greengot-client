64b19061e6d4a10a1476f162edcbe3521356496f
Grégoire Seux <grego_github@familleseux.net>
2024-07-21 20:28:48 +0200

    Rubocop for readability

--- greengot_csv2qif.rb
+++ greengot_csv2qif.rb
@@ -14,19 +14,18 @@ loop do
   break if line.nil?
   next if line =~ /^"Transaction ID",/
 
-  parsed = line.split(",")
+  parsed = line.split(',')
   transaction = {}
-  transaction["id"] = extract(parsed, 0)
-  transaction["amount"] = { "currency" => extract(parsed, 5), "value" => extract(parsed, 3).to_f }
-  transaction["status"] = extract(parsed, 1)
-  transaction["createdAt"] = extract(parsed, 2)
-  transaction["direction"] = extract(parsed, 4)
-  transaction["iban"] = extract(parsed, 6)
-  transaction["counterparty"] = extract(parsed, 7)
-  transaction["counterparty_iban"] = extract(parsed, 8)
-  transaction["payment_method"] = extract(parsed, 9)
-  transaction["reference"] = extract(parsed, 11)
-
+  transaction['id'] = extract(parsed, 0)
+  transaction['amount'] = { 'currency' => extract(parsed, 5), 'value' => extract(parsed, 3).to_f }
+  transaction['status'] = extract(parsed, 1)
+  transaction['createdAt'] = extract(parsed, 2)
+  transaction['direction'] = extract(parsed, 4)
+  transaction['iban'] = extract(parsed, 6)
+  transaction['counterparty'] = extract(parsed, 7)
+  transaction['counterparty_iban'] = extract(parsed, 8)
+  transaction['payment_method'] = extract(parsed, 9)
+  transaction['reference'] = extract(parsed, 11)
 
   transactions << transaction
 end
@@ -37,9 +36,11 @@ warn "Writing output to #{output_file}"
 
 Qif::Writer.open(output_file, 'Bank', 'dd/mm/yyyy') do |qif|
   transactions.each_with_index do |transaction, index|
-    STDERR.puts "Processing #{index+1}/#{transactions.size}"
+    warn "Processing #{index + 1}/#{transactions.size}"
 
-    raise "Payment #{transaction['id']} is not in euros but in #{transaction['amount']['currency']}" unless transaction['amount']['currency'] == 'EUR'
+    unless transaction['amount']['currency'] == 'EUR'
+      raise "Payment #{transaction['id']} is not in euros but in #{transaction['amount']['currency']}"
+    end
 
     case transaction['status']
     when 'AUTHORISED', 'COMPLETE'
@@ -50,21 +51,20 @@ Qif::Writer.open(output_file, 'Bank', 'dd/mm/yyyy') do |qif|
       raise "Unknown status #{transaction['status']} for #{transaction['id']}"
     end
 
-
-    date  = Time.parse(transaction['createdAt']).to_date
+    date = Time.parse(transaction['createdAt']).to_date
     amount = case transaction['direction']
-              when 'DEBIT'
-                - transaction['amount']['value'].to_f
-              when 'CREDIT'
-                transaction['amount']['value'].to_f
-              else
-                raise "Unknown direction #{transaction['direction']} for payment #{transaction['id']}"
-              end
+             when 'DEBIT'
+               - transaction['amount']['value'].to_f
+             when 'CREDIT'
+               transaction['amount']['value'].to_f
+             else
+               raise "Unknown direction #{transaction['direction']} for payment #{transaction['id']}"
+             end
 
     memo = [
       transaction['reference'],
       transaction['note']
-    ].compact.join(" - ")
+    ].compact.join(' - ')
 
     qif << Qif::Transaction.new(
       date: date,
@@ -74,7 +74,7 @@ Qif::Writer.open(output_file, 'Bank', 'dd/mm/yyyy') do |qif|
       payee: transaction['counterparty'],
       memo: memo,
       adress: nil,
-      category: '',
+      category: ''
     )
   end
 end
